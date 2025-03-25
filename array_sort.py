df485e3b51d91a3655e09541b3a3d175f54e7c3b
Thibault Yvard <thibaultyvard01@gmail.com>
2025-03-25 11:07:55 +0100

    Update array_sort.py

--- array_sort.py
+++ array_sort.py
@@ -1,7 +1,7 @@
 kinder = [5,32,0,8,6,1]
 minVal = kinder[0]
-for i in kinder:
-    if i < minVal:
-        minVal = i
+for pinguin in kinder:
+    if pinguin < minVal:
+        minVal = pinguin
 
 print(minVal)
